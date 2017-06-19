0c17311e0beaf7fc7852ff210b7978bce27837a9
yangbinqi <ybq@outlook.com>
2017-06-20 00:21:56 +0800

    show number

--- _4bitLEDForC595/_4bitLEDForC595.ino
+++ _4bitLEDForC595/_4bitLEDForC595.ino
@@ -2,10 +2,11 @@
     {// 0	 1	  2	   3	4	 5	  6	   7	8	 9	  A	   b	C    d	  E    F    -
     	0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90,0x8C,0xBF,0xC6,0xA1,0x86,0xFF,0xbf
     };
+    const int numerOfDigits=4;
     unsigned char LED[4];	//用于LED的4位显示缓存
-    int SCLK = 2;
-    int RCLK = 1;
-    int DIO = 0; //这里定义了那三个脚
+    int SCLK = 4;
+    int RCLK = 3;
+    int DIO = 2; //这里定义了那三个脚
     void setup ()
     {
       pinMode(SCLK,OUTPUT);
@@ -13,16 +14,53 @@
       pinMode(DIO,OUTPUT); //让三个脚都是输出状态
     }
     void loop()
-    {
-        LED[0]=1;
-    	LED[1]=2;
-    	LED[2]=3;
-    	LED[3]=4;
-    	while(1)
-    	{
-    		LED4_Display ();
-    	} 
-      
+    {clearDigits();
+    		DisplayNumber (8765);
+       //delay(300);
+    }
+
+   void clearDigits(){
+     for(int i=0;i<numerOfDigits;i++){
+         int index=0;
+         int x=2;
+          index=ceil(pow(2,i));
+         displayNumberAtIndex(0,index,false);
+      }
+    }
+
+    void DisplayNumber(int number){
+      for(int i=0;i<numerOfDigits;i++){
+         int digitalNumer=number%10;
+         int index=0;
+         int x=2;
+          index=ceil(pow(2,i));
+         displayNumberAtIndex(digitalNumer,index,true);
+         number=number/10;
+      }
+    }
+
+   /* 显示或者关闭某一位
+    *  number:index参数所在位显示的数字
+    * index:第几位,位数起始位置是1，从右开始数
+    * turnOn:显示或者关闭index所在位的LED
+   */
+    void displayNumberAtIndex(unsigned char number,int index,boolean turnOn){
+    
+      if(turnOn){
+          unsigned char *led_table;          // 查表指针
+          unsigned char i;
+          led_table = LED_0F + number;
+          i = *led_table;
+          LED_OUT(i);     
+          LED_OUT(index);    
+          digitalWrite(RCLK,LOW);
+          digitalWrite(RCLK,HIGH);
+       }
+       else
+       {
+          digitalWrite(RCLK,HIGH);
+          digitalWrite(RCLK,LOW);
+        }
     }
     
     void LED4_Display (void)
@@ -55,8 +93,9 @@
     	i = *led_table;
     	LED_OUT(i);			
     	LED_OUT(0x08);		
-        digitalWrite(RCLK,LOW);
         digitalWrite(RCLK,HIGH);
+        digitalWrite(RCLK,LOW);
+        
     }
     
     void LED_OUT(unsigned char X)
@@ -77,5 +116,6 @@
                 digitalWrite(SCLK,HIGH);
     	}
     }
+
     
 
